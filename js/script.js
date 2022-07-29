805744c85e7a8731e5b9e72d4830c2cd7c1dc3c6
shaxzodkamolxojayev-098 <shaxzodkamolxojayev@gmail.com>
2022-07-29 15:43:21 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert ("welcome to night club");
+alert ("royhatdan oting");
+
+var name = prompt("ismingiz nima:", "shaxzod");
+var age = prompt("yoshingizni kiriting:", 18);
+
+if (age >= 18) {
+  alert('night Clubga xush kelibsiz')
+}else if (age <= 17){
+  alert("shaxzod" + 'night clubga 16dan kottalar kiradi')
+};
